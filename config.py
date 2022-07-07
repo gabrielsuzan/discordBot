84f2c12a3485da92f074df4bc0b5dbd7e69d91bd
Gabriel <gabriel@multiway.com.br>
2022-07-07 16:39:57 -0300

    Updates aos novos membros

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Bot token
+TOKEN = os.getenv("DISCORD_TOKEN")
+# Id dos cargos
+NEW_USER_ROLE = os.getenv("NEW_USER_ROLE_ID")
+ARGOS_P1 = os.getenv("ARGOS_P1_ID")
+ARGOS_P2 = os.getenv("ARGOS_P2_ID")
+ARGOS_P3 = os.getenv("ARGOS_P3_ID")
+VALTAN_NM = os.getenv("VALTAN_NM_ID")
+VALTAN_HM = os.getenv("VALTAN_HM_ID")
+VYKAS_NM = os.getenv("VYKAS_NM_ID")
+VYKAS_HM = os.getenv("VYKAS_HM_ID")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
 import discord
+import config
+import psycopg2
 from discord.ext import commands
+
 intents = discord.Intents.all()
 discord.member = True
 bot = commands.Bot(command_prefix="!", intents=intents)
-TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
@@ -13,23 +14,47 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+    # channel = bot.get_channel(config.NEW_USER_CHANNEL)
+    channel = member.guild.system_channel
+    true_member_count = len([m for m in channel.guild.members if not m.bot])
+    
     embed = discord.Embed(
         title="Olá "+ member.name +"!",
-        description="Seja bem vindo ao Sfclan!",
+        description="""
+        Seja bem vindo ao Sfclan!
+        
+        Agora somos {0} membros!
+        
+        Este canal é específico para visitantes, o acesso completo ao servidor será disponibilizado após a confirmação de que seu personagem está na guild. Chame por alguém da staff para ganhar o cargo de Guildmate e dar continuição a integração na guild.
+        
+        Enquanto aguarda, leia também as {1.mention}
+        
+        Coloque o nome do seu personagem principal em {2.mention} para adiantar o processo.
+        """.format(true_member_count,
+                   discord.utils.get(channel.guild.channels, name = 'regras'),
+                   discord.utils.get(channel.guild.channels, name = 'identifique-se')),
         color=discord.Color.green()
     )
 
-    channel = bot.get_channel(994209459071094794)
-    await channel.send(embed=embed)
+    if channel is not None:
+        await channel.send(embed=embed)
     
-    role = discord.utils.get(member.guild.roles, id=994206957621760061)
-    await member.add_roles(role)
-    print("Added role '" + role.name + "' to " + member.name)
-    
+    newcomer_role = int(config.NEW_USER_ROLE)
+    role = discord.utils.get(member.guild.roles, id=newcomer_role)
+    await member.add_roles(role)    
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+    
+# @bot.command()
+# async def assign_raid_roles(ctx):
+#     await ctx.channel.send(
+#         "This is a button test",
+#         components = [
+#             discord.ui.Button(style=ButtonStyle.blue, label="Button 1")
+#         ]
+#     )
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    bot.run(config.TOKEN)
